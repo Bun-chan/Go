5b672e387c4d6d6a42c61bf6b4203b2fe9635fd2
BunChan <stephenjamesfitzgerald@yahoo.com>
2025-01-26 15:34:36 +0900

    Initial Commit

---
+++ Go/GoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoApp.swift
+//  Go
+//
+//  Created by STEPHEN FITZGERALD on 2025/01/26.
+//
+
+import SwiftUI
+
+@main
+struct GoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
